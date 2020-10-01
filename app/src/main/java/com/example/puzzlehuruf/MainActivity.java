45cc3878d9c590c3fea8202f91208ea0104cad22
nurainigita03 <nurainigita03@gmail.com>
2020-10-02 06:02:17 +0700

    new project

---
+++ app/src/main/java/com/example/puzzlehuruf/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.puzzlehuruf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.optionmenu,menu);
+
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
